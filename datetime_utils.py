a8913b7e55edc89ecb9f8b93e3ad44db59ff06c5
vikram-insights <ashokashok83436@gmail.com>
2026-09-02 23:21:55 +0530

    refactor: updated datetime_utils

--- datetime_utils.py
+++ datetime_utils.py
@@ -3,9 +3,15 @@ from datetime import datetime
 
 
 def get_datetime():
+    # 1. Fecth  the today's date and time
     today = datetime.now()
+
+    # 2. Converts the date in desired format
     current_date = today.strftime('%d-%b-%Y')
+
+    # 3. Converts the date in desired format
     current_time = today.strftime('%H:%M:%S')
+
     return current_date, current_time
     
 
