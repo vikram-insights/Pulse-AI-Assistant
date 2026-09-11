447d27e0e06b028313f09a6264122ddd0f16eb38
vikram-insights <ashokashok83436@gmail.com>
2026-09-11 23:45:27 +0530

    refactor: modified web_utils module

--- webs_utils.py
+++ webs_utils.py
@@ -13,22 +13,21 @@ websites = {
     }
 
 def open_website(name_or_url):
-    
+    # 1. Takes the clean input
     clean_input = name_or_url.strip()
 
-    
+    # 2. Open the url if present in wesbite dictionary
     if clean_input.lower() in websites:
         url = websites[clean_input.lower()]
     else:
         url = clean_input
 
-    
+    # 3. Add https if not already present in the url
     if not url.lower().startswith(("http://", "https://")):
         url = f"https://{url}"
 
     print(f"\nOpening: {url}")
     webbrowser.open_new_tab(url)
-
 
 
 
